fe4c2f7c17758d6e9b863ce2afcd163ae01f618c
gebhartn <nicholas.gebhart@gmail.com>
2019-09-17 14:15:54 -0700

    NavWrapper functionality

---
+++ src/API/data.js
@@ -0,0 +1,92 @@
+export const mainNavItems = [
+  {
+    title: "Home",
+    href: "/",
+    image:
+      "https://www.apple.com/ac/globalnav/4/en_US/images/globalnav/apple/image_large.svg"
+  },
+  { title: "Mac", href: "mac" },
+  { title: "iPad", href: "ipad" },
+  { title: "iPhone", href: "iphone" },
+  { title: "Watch", href: "watch" },
+  { title: "TV1", href: "tv" },
+  { title: "Music", href: "music" },
+  { title: "Support", href: "support" },
+  {
+    title: "Search",
+    href: "search",
+    image:
+      "https://www.apple.com/ac/globalnav/4/en_US/images/globalnav/search/image_large.svg"
+  },
+  {
+    title: "Cart",
+    href: "cart",
+    image:
+      "https://www.apple.com/ac/globalnav/4/en_US/images/globalnav/bag/image_large.svg"
+  }
+];
+
+export const subNavItems = {
+  Mac: [
+    { title: "MacBook Air" },
+    { title: "MacBook Pro" },
+    { title: "iMac" },
+    { title: "iMac Pro" },
+    { title: "Mac Pro" },
+    { title: "Mac mini" },
+    { title: "Compare" },
+    { title: "Pro Display XDR" },
+    { title: "Accessories" },
+    { title: "Catalina" }
+  ],
+  iPad: [
+    { title: "iPad Air" },
+    { title: "iPad Pro" },
+    { title: "iPad" },
+    { title: "iPad mini" },
+    { title: "Compare" },
+    { title: "Apple Pencil" },
+    { title: "Smart Keyboard" },
+    { title: "Accessories" },
+    { title: "iPadOS" }
+  ],
+  iPhone: [
+    { title: "iPhone 11 Pro" },
+    { title: "iPhone 11" },
+    { title: "iPhone Xr" },
+    { title: "iPhone 8" },
+    { title: "Compare" },
+    { title: "Apple Card" },
+    { title: "Airpods" },
+    { title: "Accessories" },
+    { title: "iOS13" }
+  ],
+  Watch: [
+    { title: "Apple Watch Series 5" },
+    { title: "Apple Watch Nike" },
+    { title: "Apple Watch Hermes" },
+    { title: "Apple Watch Edition" },
+    { title: "Apple Watch Series 3" },
+    { title: "Compare" },
+    { title: "Bands" },
+    { title: "Airpods" },
+    { title: "Accessories" },
+    { title: "watchOS" }
+  ],
+  TV: [
+    { title: "Apple TV App" },
+    { title: "Apple TV+" },
+    { title: "Apple TV 4K" },
+    { title: "Apple TV HD" },
+    { title: "Airplay" },
+    { title: "Accessories" }
+  ],
+  Music: [
+    { title: "Apple Music" },
+    { title: "Homepod" },
+    { title: "Airpods" },
+    { title: "iPod touch" },
+    { title: "Music Accessories" },
+    { title: "Gift Cards" }
+  ]
+};
